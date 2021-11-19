03f9fe28b1675ae684083a8212388ee7904abaf7
careys <sean.carey.mil@jdi.socom.mil>
2021-11-19 14:07:42 -0500

    add models to admin.py

--- index/admin.py
+++ index/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from . import models
+
+admin.site.register(models.Form)
+admin.site.register(models.User)
+admin.site.register(models.Answer)
+admin.site.register(models.Choices)
+admin.site.register(models.Questions)
+admin.site.register(models.Responses)
